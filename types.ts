c80791fd150627bc588d9f12d0380cc2133ccf56
dayunchin <126480646+dayunchin@users.noreply.github.com>
2026-02-12 13:43:41 +0800

    Delete types.ts

--- types.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Layer {
-    id: string;
-    name: string;
-    isVisible: boolean;
-    canvas: HTMLCanvasElement | null;
-}
-
-export type AIFeature = 'textToImage' | 'music' | 'story' | 'feedback';
